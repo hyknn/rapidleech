5771a6d42330462e3ba95e3768104422f48b0ca7
Henry <henrykurniawan1st@gmail.com>
2023-01-31 11:48:00 +0700

    optimize code

--- deny.php
+++ deny.php
@@ -1,10 +1,15 @@
 <?php
-
-require_once('rl_init.php');
+require_once 'rl_init.php';
 
 // Access denied page
-include(TEMPLATE_DIR.'header.php');
-echo('<div align="center"><h1>'.lang(1).'</h1><p>'.lang(2).'</p></div>');
-include(TEMPLATE_DIR.'footer.php');
+include TEMPLATE_DIR . 'header.php';
+?>
 
+<div align="center">
+    <h1><?= lang(1) ?></h1>
+    <p><?= lang(2) ?></p>
+</div>
+
+<?php
+include TEMPLATE_DIR . 'footer.php';
 ?>
--- notes.php
+++ notes.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('rl_init.php');
+
 if ($options['notes_disable']) {
 	require_once('deny.php');
 	exit();
@@ -8,26 +9,28 @@ if ($options['notes_disable']) {
 
 login_check();
 
-include(TEMPLATE_DIR.'header.php'); ?>
-<br />
-<?php
+include(TEMPLATE_DIR . 'header.php');
 
-	if (!file_exists(DOWNLOAD_DIR.basename(lang(327)).'.txt')) {
-		$temp = fopen(DOWNLOAD_DIR.basename(lang(327)).'.txt', 'w');
-		fclose($temp);
-	}
-	if (isset($_POST['notes']) && $_POST['notes']) {
-		file_put_contents(DOWNLOAD_DIR.basename(lang(327)).'.txt', $_POST['notes']);
+if (!file_exists(DOWNLOAD_DIR . basename(lang(327)) . '.txt')) {
+	$temp = fopen(DOWNLOAD_DIR . basename(lang(327)) . '.txt', 'w');
+	fclose($temp);
+}
+
+$content = file_get_contents(DOWNLOAD_DIR . basename(lang(327)) . '.txt');
+
+if (isset($_POST['notes']) && $_POST['notes']) {
+	file_put_contents(DOWNLOAD_DIR . basename(lang(327)) . '.txt', $_POST['notes']);
+	echo '<p>' . lang(325) . '</p>';
+}
+
 ?>
-	<p><?php echo lang(325); ?></p>
-<?php
-	}
-	$content = file_get_contents(DOWNLOAD_DIR.basename(lang(327)).'.txt');
-?>
+
 <div align="center">
-<form method="post" action="<?php echo $PHP_SELF;?>">
-<textarea class="notes" name="notes" rows="1" cols="1"><?php echo htmlentities($content); ?></textarea>
-<br /><input type="submit" name="submit" value="<?php echo lang(326); ?>" />
-</form>
+	<form method="post" action="<?php echo $PHP_SELF; ?>">
+		<textarea class="notes" name="notes" rows="1" cols="1"><?php echo htmlentities($content); ?></textarea>
+		<br />
+		<input type="submit" name="submit" value="<?php echo lang(326); ?>" />
+	</form>
 </div>
-<?php include(TEMPLATE_DIR.'footer.php'); ?>
+
+<?php include(TEMPLATE_DIR . 'footer.php'); ?>
